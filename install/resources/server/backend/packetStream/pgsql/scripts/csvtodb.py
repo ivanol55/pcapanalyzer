6769bd716fbf53b93a5181d947bbd87e96527d00
Ivan Mendez <ivan@ies-pc>
2021-05-19 13:05:16 +0200

    Wrong script replaced, it didn't insert into remote databases

--- install/resources/server/backend/packetStream/pgsql/scripts/csvtodb.py
+++ install/resources/server/backend/packetStream/pgsql/scripts/csvtodb.py
@@ -3,30 +3,34 @@ import psycopg2
 import os
 import configparser
 
-#Read and parse the server configuration file
+#Read and parse the agent configuration file
 config = configparser.ConfigParser()
 configFile = "../../../.my.cnf"
 config.read(configFile)
 
-#Set all of the necessary variables from the server configuration file
+#Set all the necessary variables from the parsed configuration file
 dbHost = config["client"]["host"]
 dbUser = config["client"]["user"]
 dbPassword = config["client"]["password"]
 dbDatabase = config["client"]["database"]
 basedir = config["paths"]["basedir"]
 
-#Connect to the target database, generally packetstream, where you will insert the data from the csv
-connString = "host='" + dbHost + "' dbname='" + dbDatabase + "' user='" + dbUser + "' password='" + dbPassword + "'"
+#Connect to the target remote database
+connString = "host='"  + dbHost + "' dbname='" + dbDatabase + "' user='" + dbUser + "' password='" + dbPassword + "'"
 conn = psycopg2.connect(connString)
 cursor = conn.cursor()
 
-#Read the CSV data into the connected database
-query = "COPY main(packettimestamp, machineid, sourcemac, destinationmac, sourceip, destinationip, protocol, sourceport, destinationport, info) FROM '" + basedir + "packetStream/pgsql/inputs/output.csv' WITH (FORMAT CSV, ENCODING 'UTF-8', DELIMITER ',', HEADER false, QUOTE '\"')"
-cursor.execute(query)
+#Open the csv file for reading
+csv = basedir + "packetStream/pgsql/inputs/output.csv"
+contents = open(csv, "r")
+
+#Insert the data from the CSV to the database
+query = "COPY main(packettimestamp, machineid, sourcemac, destinationmac, sourceip, destinationip, protocol, sourceport, destinationport, info) FROM STDIN WITH (FORMAT CSV, ENCODING 'UTF-8', DELIMITER ',', HEADER false, QUOTE '\"') \n"
+cursor.copy_expert(query, contents)
 cursor.close()
 conn.commit()
 conn.close()
 
-#File cleanup
+#Cleanup
 delete = basedir + "packetStream/pgsql/inputs/output.csv"
 os.remove(delete)
